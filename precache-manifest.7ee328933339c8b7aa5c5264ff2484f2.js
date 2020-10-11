c9823180b7eab8af016e2f5a493fce5f2cbda368
premtechie <premtechie07@gmail.com>
2020-10-11 23:32:45 +0530

    Updates

---
+++ precache-manifest.7ee328933339c8b7aa5c5264ff2484f2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c9500a9091acdf7578e5b6062250a7",
+    "url": "/your-choice-app/index.html"
+  },
+  {
+    "revision": "45242ddcfea2579cf790",
+    "url": "/your-choice-app/static/css/2.34c0de18.chunk.css"
+  },
+  {
+    "revision": "b93fc493b511cc99b56a",
+    "url": "/your-choice-app/static/css/main.af37bec8.chunk.css"
+  },
+  {
+    "revision": "45242ddcfea2579cf790",
+    "url": "/your-choice-app/static/js/2.1ed8f386.chunk.js"
+  },
+  {
+    "revision": "ee24df130e0771fa560a257ad60b5b55",
+    "url": "/your-choice-app/static/js/2.1ed8f386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93fc493b511cc99b56a",
+    "url": "/your-choice-app/static/js/main.5f61f402.chunk.js"
+  },
+  {
+    "revision": "fccc46d5ba7da067e408",
+    "url": "/your-choice-app/static/js/runtime-main.8d492dcb.js"
+  },
+  {
+    "revision": "0881a0402a361abbd3a325afe8abd4fc",
+    "url": "/your-choice-app/static/media/image1.0881a040.png"
+  },
+  {
+    "revision": "6b0b15d981526771dced3f5c9640bcfa",
+    "url": "/your-choice-app/static/media/image1.6b0b15d9.png"
+  },
+  {
+    "revision": "f74102e1ba753e43092d9a5cedd8bec6",
+    "url": "/your-choice-app/static/media/image1.f74102e1.png"
+  },
+  {
+    "revision": "95aa6a885d199ba4507a0732dc2279ea",
+    "url": "/your-choice-app/static/media/image2.95aa6a88.png"
+  },
+  {
+    "revision": "d4ba84c4131a2f6cd10778dd4aad0eab",
+    "url": "/your-choice-app/static/media/image2.d4ba84c4.png"
+  },
+  {
+    "revision": "f1798bd55f00b120495cb6f2a3fd83a4",
+    "url": "/your-choice-app/static/media/image2.f1798bd5.png"
+  },
+  {
+    "revision": "b3e5adc5e9dc77d3c065748af90f8597",
+    "url": "/your-choice-app/static/media/image3.b3e5adc5.png"
+  },
+  {
+    "revision": "dfc6cf6cd7a05f41ba1c6a05b9e7ed36",
+    "url": "/your-choice-app/static/media/image4.dfc6cf6c.png"
+  },
+  {
+    "revision": "9bb4eff61d12087648559078923e3508",
+    "url": "/your-choice-app/static/media/image5.9bb4eff6.png"
+  },
+  {
+    "revision": "eb9b429a00cb8e989999fc399bc056d4",
+    "url": "/your-choice-app/static/media/yourchoice.eb9b429a.png"
+  }
+]);
